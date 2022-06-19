8150d026d2a70133f4b8b4f5a87b5dfddbc6da01
Mohammed Shamsi <mshamsi2@outlook.com>
2022-06-18 21:31:43 -0500

    tiny more fixes

--- src/rvector/mod.rs
+++ src/rvector/mod.rs
@@ -1,7 +1,7 @@
-pub mod rvector;
 pub mod rvector_addition;
 pub mod rvector_dot_multiplication;
 pub mod rvector_equality;
+pub mod rvector_std;
 pub mod rvector_subtraction;
 
 use crate::OpErrors;
